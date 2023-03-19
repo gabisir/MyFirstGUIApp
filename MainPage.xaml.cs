50bdb0ee2da4f84f7da00cdd53e5a5587e385a47
gabisir <gabrijel802@gmail.com>
2023-03-19 17:32:17 +0100

    Possible to change numbers to different base

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -24,12 +24,26 @@ namespace MyFirstGUIApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        void ReturnBase(int numberBase)
+        {
+            if (numberBase == 1)
+                radioButton1.IsChecked = true;
+            else if (numberBase == 2)
+                radioButton2.IsChecked = true;
+            else if (numberBase == 3)
+                radioButton3.IsChecked = true;
+            else if (numberBase == 4)
+                radioButton4.IsChecked = true;
+        }
         public MainPage()
         {
             this.InitializeComponent();
+            textBox1.Text="";
+            textBox2.Text="";
         }
         int result=0, first=0, second=0;
-
+        int numberBase = 1;
+        bool baseChange = false;
         private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
         {
 
@@ -48,33 +62,48 @@ namespace MyFirstGUIApp
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
             //addition
+            if (baseChange == true)
+                numberBase = 1;
+            radioButton1.IsChecked = true;
             first = Int32.Parse(textBox1.Text);
             second = Int32.Parse(textBox2.Text);
             result = first + second;
             textBlockAns.Text=result.ToString();
+            ReturnBase(numberBase);
         }
 
         private void Button2_Click(object sender, RoutedEventArgs e)
         {
             //subtraction
+            if (baseChange == true)
+                numberBase = 1;
+            radioButton1.IsChecked = true;
             first = Int32.Parse(textBox1.Text);
             second = Int32.Parse(textBox2.Text);
             result = first - second;
             textBlockAns.Text = result.ToString();
+            ReturnBase(numberBase);
         }
 
         private void Button3_Click(object sender, RoutedEventArgs e)
         {
             //multiplication
+            if (baseChange == true)
+                numberBase = 1;
+            radioButton1.IsChecked = true;
             first = Int32.Parse(textBox1.Text);
             second = Int32.Parse(textBox2.Text);
             result = first * second;
             textBlockAns.Text = result.ToString();
+            ReturnBase(numberBase);
         }
 
         private void Button4_Click(object sender, RoutedEventArgs e)
         {
             //division
+            if (baseChange == true)
+                numberBase = 1;
+            radioButton1.IsChecked = true;
             first = Int32.Parse(textBox1.Text);
             second = Int32.Parse(textBox2.Text);
             if (second == 0)
@@ -84,40 +113,58 @@ namespace MyFirstGUIApp
                 result = first / second;
                 textBlockAns.Text = result.ToString();
             }
+            ReturnBase(numberBase);
         }
         private void radioButton1_Checked(object sender, RoutedEventArgs e)
         {
             //decimal
+            if (baseChange == true)
+                numberBase = 1;
             if(radioButton1.IsChecked==true)
             {
                 textBlockAns.Text = Convert.ToString(result, 10);
+                textBox1.Text = Convert.ToString(first, 10);
+                textBox2.Text = Convert.ToString(second, 10);
             }
+            baseChange = true;
         }
 
         private void radioButton2_Checked(object sender, RoutedEventArgs e)
         {
             //binary
+            numberBase = 2;
             if (radioButton2.IsChecked == true)
             {
                 textBlockAns.Text = Convert.ToString(result, 2);
+                textBox1.Text = Convert.ToString(first, 2);
+                textBox2.Text = Convert.ToString(second, 2);
+                baseChange = false;
             }
         }
 
         private void radioButton3_Checked(object sender, RoutedEventArgs e)
         {
             //hexadecimal
+            numberBase = 3;
             if (radioButton3.IsChecked == true)
             {
                 textBlockAns.Text = Convert.ToString(result, 16);
+                textBox1.Text = Convert.ToString(first, 16);
+                textBox2.Text = Convert.ToString(second, 16);
+                baseChange = false;
             }
         }
 
         private void radioButton4_Checked(object sender, RoutedEventArgs e)
         {
             //octal
+            numberBase = 4;
             if (radioButton4.IsChecked == true)
             {
                 textBlockAns.Text = Convert.ToString(result, 8);
+                textBox1.Text = Convert.ToString(first, 8);
+                textBox2.Text = Convert.ToString(second, 8);
+                baseChange = false;
             }
         }
     }
